c84da6c991cb576c41471579a0c67ef47327219f
tarun824 <tarun.rs82004@example.com>
2025-01-15 22:03:33 +0530

    Revert "Implemented Tinder like swipe feature for connections"
    This reverts commit 0c2509e5d666a24b349a922bb90a417f726dc1fb.

--- frontend/src/components/UserCard.jsx
+++ frontend/src/components/UserCard.jsx
@@ -19,29 +19,27 @@ const UserCard = ({ user }) => {
   };
 
   return (
-    <div className="w-[400px] h-[600px] flex items-center justify-center  shadow-lg">
-      <div className="card bg-base-300 w-96 shadow-xl">
-        <figure>
-          <img src={user.photoUrl} alt="photo" />
-        </figure>
-        <div className="card-body">
-          <h2 className="card-title">{firstName + " " + lastName}</h2>
-          {age && gender && <p>{age + ", " + gender}</p>}
-          <p>{about}</p>
-          <div className="card-actions justify-center my-4">
-            <button
-              className="btn btn-primary"
-              onClick={() => handleSendRequest("ignored", _id)}
-            >
-              Ignore
-            </button>
-            <button
-              className="btn btn-secondary"
-              onClick={() => handleSendRequest("interested", _id)}
-            >
-              Interested
-            </button>
-          </div>
+    <div className="card bg-base-300 w-96 shadow-xl">
+      <figure>
+        <img src={user.photoUrl} alt="photo" />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">{firstName + " " + lastName}</h2>
+        {age && gender && <p>{age + ", " + gender}</p>}
+        <p>{about}</p>
+        <div className="card-actions justify-center my-4">
+          <button
+            className="btn btn-primary"
+            onClick={() => handleSendRequest("ignored", _id)}
+          >
+            Ignore
+          </button>
+          <button
+            className="btn btn-secondary"
+            onClick={() => handleSendRequest("interested", _id)}
+          >
+            Interested
+          </button>
         </div>
       </div>
     </div>
