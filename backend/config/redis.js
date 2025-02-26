b9e1ab4ff53110dc6af93e98fd9e82ca4cea34c4
tarun-huzl <tarun@huzl.in>
2025-02-26 21:12:45 +0530

    Resolved DeprecationWarning and made profile edit working

--- backend/config/redis.js
+++ backend/config/redis.js
@@ -3,15 +3,15 @@ const { promisify } = require("util");
 
 // Create Redis client
 const redisClient = redis.createClient({
-  // if you are using docker to spin up the redis the host should be localhost and port should be 6379 
+  // if you are using docker to spin up the redis the host should be localhost and port should be 6379
   host: "localhost",
   port: 6379,
 });
 
 // Promisify Redis methods for easier use
-const getAsync = promisify(redisClient.get).bind(redisClient);
-const setAsync = promisify(redisClient.set).bind(redisClient);
-const delAsync = promisify(redisClient.del).bind(redisClient);
+const getAsync = redisClient.get.bind(redisClient);
+const setAsync = redisClient.set.bind(redisClient);
+const delAsync = redisClient.del.bind(redisClient);
 
 // Handle connection events
 redisClient.on("connect", () => {
